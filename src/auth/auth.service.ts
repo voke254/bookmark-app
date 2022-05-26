2901da37965064d12bb30a592acab2d387a45452
voke254 <kevinnjorog83@gmail.com>
2022-05-26 18:34:56 +0300

    Add validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon2 from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 
 @Injectable()
@@ -13,23 +14,40 @@ export class AuthService {
         const hash = await argon2.hash(dto.password);
         
         // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
             
-        });
+            delete user.hash
+            // return the saved user
+            return {
+                "User created successfully": user
+            };
+            
+        } catch (error) {
+          if (error instanceof PrismaClientKnownRequestError) {
+              if (error.code === 'P2002') {
+                  throw new ForbiddenException('Email already in use')
+              }
+          }
+          throw error;
+        }
         
-        delete user.hash
-        // return the saved user
-
-        return {
-            "User created successfully": user
-        };
     }
     
-    signin() {
+    async signin(dto: AuthDto) {
+
+        // find the user by email
+        // if user does not exist throw excetion
+
+        // compare password
+        // if password incorrect throw exception
+
+        // send back the user
         return {msg:'I am signed in'};
     }
 }
